ecffd81664962d648b13df6aa5fbb650332bc658
sherif4 <dr.sherif.ashraf4@gmail.com>
2025-11-19 21:53:45 +0200

    new commit

--- src/main/java/com/clinic_project/clinic_system/business_layer/services/MessagingService.java
+++ src/main/java/com/clinic_project/clinic_system/business_layer/services/MessagingService.java
@@ -5,11 +5,7 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class MessagingService {
-    public static final String ACCOUNT_SID = "AC3b52cb83e48d14f42926157529c11557";
-    public static final String AUTH_TOKEN = "485b7879938dc7f3c28083ea49769b3c";
-
     public static void main(String[] args) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message.creator(
                         new PhoneNumber("+201015597857"), // To number
